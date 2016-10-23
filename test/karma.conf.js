6cbf5d825abedf282fe97217e8332e6d88ee3023
于美琪 <986823092@qq.com>
2016-10-23 23:49:36 +0800

    定制开发案例页面完善

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -40,6 +40,7 @@ module.exports = function(config) {
       'bower_components/tap-listener/tap-listener.js',
       'bower_components/unidragger/unidragger.js',
       'bower_components/flickity/js/index.js',
+      'bower_components/owl.carousel/dist/owl.carousel.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
